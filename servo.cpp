63eec5d81062668fa2e0f353fef1041d1c226b9c
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-05-23 18:25:28 -0400

    Update servo.cpp

--- servo.cpp
+++ servo.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <wiringPi.h>
-#include <softPwm.h>
+// #include <wiringPi.h>
+// #include <softPwm.h>
+
+#include <includes/wiringPi.h>
+#include <includes/softPwm.h>
 
 #include <stdio.h>
 
-#define mapRange(a1, a2, b1, b2, s) (b1 + (s - a1) * (b2 - b1) / (a2 - a1))
+#define mapRange(a1, a2, b1, b2, s) (b1 + (((s - a1) * (b2 - b1)) / (a2 - a1)))
 float x;
 
 int main(void)
@@ -16,13 +19,13 @@ int main(void)
     while (1)
     {
 
-        for (x; x < 28; x++)
+        for (x = 0; x <= 28; x++)
         {
             softPwmWrite(pin, mapRange(0, 28, 0, 360, x));
             delay(700);
             printf("%d = %g \n", x, mapRange(0, 28, 0, 360, x));
         }
-        for (x; x > 0; x--)
+        for (x = 0; x = > 0; x--)
         {
             softPwmWrite(pin, mapRange(0, 28, 0, 360, x));
             delay(700);

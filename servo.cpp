95dec07046e43721c890203298e8a9e5ec213d9f
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-05-23 11:00:20 -0400

    Update servo.cpp

--- servo.cpp
+++ servo.cpp
@@ -6,13 +6,10 @@ Servo myservo; // create servo object to control a servo
 
 int pos = 0; // variable to store the servo position
 
-void setup()
+int main(void)
 {
     myservo.attach(0); // attaches the servo on pin 9 to the servo object
-}
 
-void loop()
-{
     for (pos = 0; pos <= 180; pos += 1)
     { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree

c3a82f27bbacc915985cad809045a6ffd1e80804
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-05-23 12:32:05 -0400

    Update servo.c

--- servo.c
+++ servo.c
@@ -8,12 +8,12 @@ int main(void)
     softPwmCreate(pin, 0, 30);
     int x;
 
-    for (x, x < 30, x++)
+    for (x; x < 30; x++)
     {
         softPwmWrite(pin, x);
         delay(100);
     }
-    for (x, x > 0, x--)
+    for (x; x > 0; x--)
     {
         softPwmWrite(pin, x);
         delay(100);

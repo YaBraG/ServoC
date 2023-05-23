e412be8135e15f7e620565c8cd23fc1ff54e3315
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-05-23 11:24:24 -0400

    Update servo.c

--- servo.c
+++ servo.c
@@ -1,11 +1,23 @@
 #include <wiringPi.h>
 #include <softServo.h>
 
-int main(void)
+int main()
 {
     wiringPiSetup();
+
     softServoSetup(0, 1, 2, 3, 4, 5, 6, 7);
 
     softServoWrite(0, 500);
-    delay(10);
+    /*
+        softServoWrite (1, 1000) ;
+        softServoWrite (2, 1100) ;
+        softServoWrite (3, 1200) ;
+        softServoWrite (4, 1300) ;
+        softServoWrite (5, 1400) ;
+        softServoWrite (6, 1500) ;
+        softServoWrite (7, 2200) ;
+    */
+
+    for (;;)
+        delay(10);
 }

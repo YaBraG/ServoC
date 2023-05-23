665ce28076e2e699f7cf91b0b620d7fede86179c
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-05-23 12:29:41 -0400

    q

--- servo.c
+++ servo.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
-
 #include <wiringPi.h>
-#include "softServo.h"
+#include <softPWM.h>
 
-int main()
+int main(void)
 {
-    if (wiringPiSetup() == -1)
+    int pin = 0;
+    softPwmCreate(pin, 0, 30);
+    int x;
+
+    for (x, x < 30, x++)
     {
-        fprintf(stdout, "oops: %s\n", strerror(errno));
-        return 1;
+        softPwmWrite(pin, x);
+        delay(100);
     }
-
-    softServoSetup(0, 1, 2, 3, 4, 5, 6, 7);
-
-    softServoWrite(0, 0);
-    /*
-      softServoWrite (1, 1000) ;
-      softServoWrite (2, 1100) ;
-      softServoWrite (3, 1200) ;
-      softServoWrite (4, 1300) ;
-      softServoWrite (5, 1400) ;
-      softServoWrite (6, 1500) ;
-      softServoWrite (7, 2200) ;
-    */
-
-    for (;;)
-        delay(10);
+    for (x, x > 0, x--)
+    {
+        softPwmWrite(pin, x);
+        delay(100);
+    }
 }

8f2dffa2cb700166e7e4a8bd03cf62e6218bcb29
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-05-23 11:45:17 -0400

    Update servo.c

--- servo.c
+++ servo.c
@@ -1,23 +1,16 @@
 #include <wiringPi.h>
-#include <softServo.h>
+#include <stdio.h>
 
-int main()
+int main(void)
 {
-    wiringPiSetup();
-
-    softServoSetup(0, 1, 2, 3, 4, 5, 6, 7);
-
-    softServoWrite(0, 500);
-    /*
-        softServoWrite (1, 1000) ;
-        softServoWrite (2, 1100) ;
-        softServoWrite (3, 1200) ;
-        softServoWrite (4, 1300) ;
-        softServoWrite (5, 1400) ;
-        softServoWrite (6, 1500) ;
-        softServoWrite (7, 2200) ;
-    */
-
-    for (;;)
-        delay(10);
+    printf("Raspberry Pi wiringPi test program\n");
+    wiringPiSetupGpio();
+    pinMode(18, PWM_OUTPUT);
+    pwmSetMode(PWM_MODE_MS);
+    pwmSetRange(2000);
+    pwmSetClock(192);
+    pwmWrite(18, 150);
+    delay(1000);
+    pwmWrite(18, 200);
+    return 0;
 }
